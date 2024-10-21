ff12ccf6b8e48b0248d8ab83b8864a97012e5f09
MatheusVaghetti <146997137+MatheusVaghetti@users.noreply.github.com>
2024-10-21 13:53:04 +0000

    atualizado

--- src/main/java/br/dev/matheus/webservice/CarroController.java
+++ src/main/java/br/dev/matheus/webservice/CarroController.java
@@ -3,8 +3,6 @@ package br.dev.matheus.webservice;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
