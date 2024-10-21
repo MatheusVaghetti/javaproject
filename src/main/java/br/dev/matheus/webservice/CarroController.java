0f09fda0aec992960807ec9e1f8ed9e97b87199d
MatheusVaghetti <146997137+MatheusVaghetti@users.noreply.github.com>
2024-10-21 13:59:26 +0000

    att

--- src/main/java/br/dev/matheus/webservice/CarroController.java
+++ src/main/java/br/dev/matheus/webservice/CarroController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarroController {
-
     private CarroManager carro = new CarroManager();
 
     @GetMapping("api/carros")
--- src/main/java/br/dev/matheus/webservice/CarroManager.java
+++ src/main/java/br/dev/matheus/webservice/CarroManager.java
@@ -9,6 +9,8 @@ public class CarroManager {
         this.carros = new ArrayList<>();
         this.carros.add(new Carro("Nivus", "VW"));
         this.carros.add(new Carro("Tucson", "Hyunday"));
+        this.carros.add(new Carro("Camaro", "Chevrolet"));
+        this.carros.add(new Carro("Corrola", "Toyota"));
     }
     public List<Carro> getCarros(){
         return this.carros;
